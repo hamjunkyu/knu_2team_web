a0fe9ded2909634fe08195b8764555b322a468cd
JIHEEYOU <yujihui5@gmail.com>
2024-08-13 13:29:23 +0900

    branch 생성

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static("public"));
-
+console.log("exec express");
 app.use("/api", apiController);
 
 app.post("/api/login", (req, res) => {
